4570960b0e8110f9c36d7ddd3e2930fee1de005d
nemo <huynhviha1703@gmail.com>
2021-09-08 20:54:25 +0700

    Section 18: Node.js

---
+++ intro-to-node/index.js
@@ -0,0 +1,6 @@
+const superheros = require('superheroes')
+const supervillains = require('supervillains')
+
+console.log('Superheroes name: ', superheros.random())
+console.log('Supervillains name: ', supervillains.random())
+
